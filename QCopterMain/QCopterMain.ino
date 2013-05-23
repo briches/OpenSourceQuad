7a68cbdca3dbfa8ca2c8725700f66ad37f507c71
Brandon Riches <briches@ualberta.ca>
2013-05-22 19:57:04 -0500

    Update QCopterMain.ino

--- QCopterMain/QCopterMain.ino
+++ QCopterMain/QCopterMain.ino
@@ -2,17 +2,10 @@
 #include <MMA8453_n0m1.h>
 #include <OseppGyro.h>
 #include <I2C.h>
-
 Control QCopter;
-
-void setup()
-{
+void setup(){
   Serial.begin(9600);
-  QCopter.initSensor();
-}
-  
-void loop()
-{
+  QCopter.initSensor();}
+void loop(){
   QCopter.updateData_State();
-  Serial.println(QCopter.Data_State.heading);
-}
+  Serial.println(QCopter.Data_State.heading);}
