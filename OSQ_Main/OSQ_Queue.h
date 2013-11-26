44e5ed15522b3e6cae893a6d3dd6a8a927864be9
andyrewsu <and-y-rew@hotmail.com>
2013-11-26 09:47:04 -0700

    Queue Library Update
    Added modifiable memory allocation, header update

--- OSQ_Main/OSQ_Queue.h
+++ OSQ_Main/OSQ_Queue.h
@@ -8,9 +8,9 @@
 	Date		: November 2013
 	License		: GNU Public License
 
-	This header provides a MAXIMUM_SIZE byte queue to store incoming data.
-	The boolean returns from the put/get functions can be used to determine
-	if data was stored successfully.
+	This header provides a MAXIMUM_SIZE byte circular queue to store incoming 
+	data. The boolean returns from the put/get functions can be used to 
+	determine if data was stored successfully.
 
 	Copyright (C) 2013  Andrew Coulthard
 
@@ -32,7 +32,6 @@
 #ifndef OSQ_QUEUE_H_INCLUDED
 #define OSQ_QUEUE_H_INCLUDED
 
-#define MAXIMUM_SIZE 10 // Size of queue in bytes
 #define MINIMUM_SIZE 1
 
 typedef struct Queue_t
@@ -41,7 +40,8 @@ typedef struct Queue_t
 	
 		uint8_t  size,
                  head,
-                 tail;
+                 tail,
+				 maxsize;
         
         uint8_t * memory;
         
@@ -58,15 +58,16 @@ typedef struct Queue_t
 /* Constructor, initializes queue memory array and initial values for
 queue variables. Will set err == 1 if memory is unable to be allocated.
 */
-Queue_t :: Queue_t(uint8_t max, uint8_t min) 
+Queue_t :: Queue_t(uint8_t max) 
 {
 
 	size = 0;
 	head = 0;
 	tail = 0;
 	err = 0;
+	maxsize = max;
 
-	memory = (uint8_t *)malloc(sizeof(uint8_t)*MAXIMUMSIZE);
+	memory = (uint8_t *)malloc(sizeof(uint8_t)*maxsize);
 	if(!memory) {
 		err = 1;
 	}
@@ -87,14 +88,14 @@ is available. Will return true/false based on memory availability.
 boolean qPutData(uint8_t data) 
 {
 
-	if (size >= MAXIMUM_SIZE) 
+	if (size >= maxsize) 
 	{
 		return false;
 	}
 	else
 	{
 		*(memory + head) = data;
-		head = (head + 1) % MAXIMUM_SIZE
+		head = (head + 1) % maxsize
 		size++;
 		return true;
 	}
@@ -114,7 +115,7 @@ boolean qGetData(uint8_t &data)
 	else
 	{
 		data = *(memory + tail);
-		tail = (tail + 1) % MAXIMUM_SIZE
+		tail = (tail + 1) % maxsize
 		size--;
 		return true;
 	}
